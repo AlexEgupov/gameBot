32c1bc76a9862306ad30b814e2b67379a69e5188
AlexEgupov <alex.egupov7@gmail.com>
2022-06-03 15:54:30 +0300

    test script.js gameBotTest

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 'use strict';
 
 const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
+    return !isNaN(parseFloat(num)) && isFinite(num) && num != 0;
 };
 
 function gameBot(counter) {
     let x = Math.ceil(Math.random() * 100);
     function compare() {
-        const userNumber = prompt('Угадай число от 1 до 100');
+        let userNumber = prompt('Угадай число от 1 до 100');
 
-        if (userNumber == null) {
-            alert('Спасибо за игру, до свидания :)');
-        } else if (counter === 0) {
-            if (true === confirm('Попытки закончились, хотите сыграть еще?')) {
+        function numCheckout(condition, message) {
+
+            if (condition) {
+                counter--;
+                confirm(message);
+                compare();
+            }
+        };
+
+        function results(condition, message1, message2) {
+
+            if (condition) {
+                confirm(message1);
+            } else if (message1) {
                 gameBot(9);
-            } else { alert('Спасибо за игру, до свидания :)') }
-        } else if (!isNumber(userNumber)) {
-            counter--;
-            alert('Введи число! Осталось попыток: ' + (counter + 1));
-            compare();
-        } else if (userNumber < x) {
-            counter--;
-            alert('Загаданное число больше. Осталось попыток: ' + (counter + 1));
-            compare();
-        } else if (userNumber > x) {
-            counter--;
-            alert('Загаданное число меньше. Осталось попыток: ' + (counter + 1));
-            compare();
-        } else if (userNumber == x) {
-            if (true == confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
-                gameBot(9);
-            } else { alert('Спасибо за игру, до свидания :)') }
-        }
+            } else { confirm(message2) };
+        };
+
+        if (userNumber == null) { confirm('Спасибо за игру, до свидания :)') };
+        numCheckout(!isNumber(userNumber), 'Введи число! Осталось попыток: ' + counter);
+        numCheckout(userNumber < x, 'Загаданное число больше. Осталось попыток: ' + counter);
+        numCheckout(userNumber > x, 'Загаданное число меньше. Осталось попыток: ' + counter);
+        results((userNumber == x), confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?'), 'Спасибо за игру, до свидания :)');
+        results(counter === 0, confirm('Попытки закончились, хотите сыграть еще?'), 'Спасибо за игру, до свидания :)');
     }
 
     compare();
